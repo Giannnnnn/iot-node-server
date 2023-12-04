c0c0a121f459f8d58a1b8ebdfa30cf53d4e63270
Gian <giremio5@gmail.com>
2023-12-04 06:24:37 -0300

    api-get-and-updates-in-general

--- index.js
+++ index.js
@@ -15,24 +15,20 @@ const database = 'test';
 const waterLevelSchema = new mongoose.Schema({
     date: String,
     value: String,
-    id: Number,
 });
 
 const soilMoistureSchema = new mongoose.Schema({
     date: String,
     value: String,
-    id: Number,
 });
 
 const waterPumpActivationSchema = new mongoose.Schema({
     date: String,
-    id: Number,
 });
 
 const Waterlevel = mongoose.model('WaterLevel', waterLevelSchema);
 const SoilMoisture = mongoose.model('SoilMoisture', soilMoistureSchema);
 const WaterPumpActivation = mongoose.model('WaterPumpActivation', waterPumpActivationSchema);
-
 
 class Database {
     constructor() {
@@ -45,9 +41,6 @@ class Database {
             .connect(`mongodb://user1:user1@193.133.195.176:27017/waterLevel?authSource=admin`)
             .then(() => {
                 console.log('Database connection successful');
-
-
-
             })
             .catch((err) => {
                 console.error('Database connection failed');
@@ -62,52 +55,54 @@ class Database {
             username: 'user1',
             password: 'user1',
             reconnectPeriod: 1000,
-        })
-
-        const topic = 'distance'
+        });
 
         client.on('connect', () => {
-            console.log('Connected')
+            console.log('Connected');
 
-            client.subscribe([topic], () => {
-                console.log(`Subscribe to topic '${topic}'`)
-                client.publish(topic, 'nodejs mqtt test', { qos: 0, retain: false }, (error) => {
-                    if (error) {
-                        console.error(error)
-                    }
-                })
-            })
-        })
+            client.subscribe('distance');
+            client.subscribe('moisture');
+            client.subscribe('waterPumpActivation');
+        });
 
         client.on('message', (topic, payload) => {
-            console.log(topic);
-            console.log(payload);
+            console.log("Payload:" + payload);
+      
+            const currentDate = new Date();
 
             const waterLevelLog = new Waterlevel({
-                date: new Date().toLocaleDateString('en-GB').toString(),
+                date: currentDate,
                 value: payload.toString(),
-                id: 1,
             });
-            const soilMoistureLog = new SoilMoisture({
 
-            })
+            const soilMoistureLog = new SoilMoisture({
+                date: currentDate,
+                value: payload.toString(),
+            });
 
             const waterPumpActivationLog = new WaterPumpActivation({
+                date: currentDate,
+                value: " Ativação da Bomba d'água",
+            });
 
-            })
-            
-            console.log('Received Message:', topic, payload.toString())
-            // waterLevelLog.save()
-            //     .then(() => {
-            //         console.log('Water Level State saved at MongoDB');
-            //        // mongoose.disconnect();
-            //     })
-            //     .catch((error) => {
-            //         console.error(error);
-            //     });
-        })
-
-      
+            switch (topic) {
+                case 'distance':
+                    waterLevelLog.save()
+                        .then(() => console.log('Water Level State saved at MongoDB'))
+                        .catch((error) => console.error(error));
+                    break;
+                case 'moisture':
+                    soilMoistureLog.save()
+                        .then(() => console.log('Soil Moisture State saved at MongoDB'))
+                        .catch((error) => console.error(error));
+                    break;
+                case 'waterPumpActivation':
+                    waterPumpActivationLog.save()
+                        .then(() => console.log('Water Pump Activation Log saved at MongoDB'))
+                        .catch((error) => console.error(error));
+                    break;
+            }
+        });
 
         process.on('SIGINT', () => {
             mongoose.connection.close(() => {
@@ -116,21 +111,21 @@ class Database {
             });
         });
     }
-
 }
 
 module.exports = new Database();
-const app = express()
+
+const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('{"text":"Hello World!"}')
-})
+    res.json({ text: 'Hello World!' });
+});
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
-    res.status(500).send('Something went wrong!');
+    res.status(500).json({ error: 'Something went wrong!' });
 });
 
 app.get('/average-water-level', async (req, res) => {
@@ -158,19 +153,18 @@ app.get('/average-soil-moisture', async (req, res) => {
 app.get('/last-water-pump-activations', async (req, res) => {
     try {
         const lastActivations = await WaterPumpActivation.find().sort({ date: -1 }).limit(10);
-        res.json({ lastActivations });
+        res.json(lastActivations);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
 
-// Function to calculate average
 function calculateAverage(array) {
     const sum = array.reduce((acc, value) => acc + value, 0);
     return sum / array.length;
 }
 
 app.listen(3000, () => {
-    console.log(`Example app listening on port ${3000}`)
-})
+    console.log(`Example app listening on port ${3000}`);
+});
